5da35783c21a0d018eb3ca0964fd6b2a5a83e5d6
Egor M <2201136@flourbluffschools.org>
2019-04-25 14:00:51 -0500

    slowed walk anim

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ x = 30
 y = 30
 clock = pygame.time.Clock()
 walk_anim = 0
-
+f = 0
 while not done:
     screen.fill((255, 255, 255))
     idle_image = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_idle with gun_0.png')
@@ -17,23 +17,25 @@ while not done:
     walk_image2 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_1.png')
     walk_image3 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_2.png')
     walk_image4 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_3.png')
-    walk_images = [walk_image1, walk_image1, walk_image1, walk_image1, walk_image2, walk_image2, walk_image2, walk_image2, walk_image3, walk_image3, walk_image3, walk_image3, walk_image4, walk_image4, walk_image4, walk_image4]
+    walk_images = [walk_image1, walk_image2, walk_image3, walk_image4]
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP] or pressed[pygame.K_DOWN] or pressed[pygame.K_LEFT] or pressed[pygame.K_RIGHT]:
-        screen.blit(walk_images[walk_anim%16],(x,y))
-        walk_anim += 1
+    if pressed[pygame.K_w] or pressed[pygame.K_s] or pressed[pygame.K_a] or pressed[pygame.K_d]:
+        f += .20
+        walk_anim = int(f)
+        screen.blit(walk_images[walk_anim%4],(x,y))
+        
     else:
         screen.blit(idle_image,(x,y))
-    if pressed[pygame.K_UP]: 
+    if pressed[pygame.K_w]: 
         y -= 3
-    if pressed[pygame.K_DOWN]: 
+    if pressed[pygame.K_s]: 
         y += 3
-    if pressed[pygame.K_LEFT]: 
+    if pressed[pygame.K_a]: 
         x -= 3
-    if pressed[pygame.K_RIGHT]: 
+    if pressed[pygame.K_d]: 
         x += 3
     pygame.display.flip()
     clock.tick(60)
