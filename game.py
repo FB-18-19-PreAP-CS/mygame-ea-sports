14948d8ca39656203737f2f8361c89f0833ed814
Egor M <2201136@flourbluffschools.org>
2019-04-26 13:23:18 -0500

    moved images out of loop

--- game.py
+++ game.py
@@ -10,18 +10,18 @@ y = 30
 clock = pygame.time.Clock()
 walk_anim = 0
 f = 0
+idle_image = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_idle with gun_0.png')
+walk_image1 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_0.png')
+walk_image2 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_1.png')
+walk_image3 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_2.png')
+walk_image4 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_3.png')
+r_walk_image1 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_0_reverse.png')
+r_walk_image2 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_1_reverse.png')
+r_walk_image3 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_2_reverse.png')
+r_walk_image4 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_3_reverse.png')
+
 while not done:
     screen.fill((255, 255, 255))
-    idle_image = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_idle with gun_0.png')
-    walk_image1 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_0.png')
-    walk_image2 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_1.png')
-    walk_image3 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_2.png')
-    walk_image4 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_3.png')
-    r_walk_image1 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_0_reverse.png')
-    r_walk_image2 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_1_reverse.png')
-    r_walk_image3 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_2_reverse.png')
-    r_walk_image4 = pygame.image.load('images/Cowboy 4 HiRes/Cowboy4_walk with gun_3_reverse.png')
-
     r_walk_images = [r_walk_image1, r_walk_image2, r_walk_image3, r_walk_image4]
     walk_images = [walk_image1, walk_image2, walk_image3, walk_image4]
 
